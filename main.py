0ab8dcd551dddf74d285b0b9a7956a504abb5952
Just1a2Noob <kamikira.hd@gmail.com>
2024-05-25 20:54:37 +0700

    Add create_report: Creates a report based on the findings of the text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+    return file_contents
 
 # Uses split() to creating a list of words and 
 # if the length of an index list is >0 then add 1 to the counter
@@ -15,3 +15,23 @@ def number_of_words():
         
         return n_words
 
+def number_of_letters():
+    file_contents = main()
+    file_contents = file_contents.lower()
+    letters_dict = {}
+    for i in file_contents:
+        if i != " " and i.isalpha() == True:
+            if i in letters_dict:
+                letters_dict[i] += 1
+            else:
+                letters_dict[i] = 1
+    return letters_dict
+
+
+def create_report():
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(number_of_words(), "words found in the document")
+    letters_dict = number_of_letters()
+    sort_letters_dict = sorted(letters_dict.items(), key=lambda x:x[1], reverse=True)
+    for d in sort_letters_dict:
+        print("The", d[0], "character was found", d[1], "times")
